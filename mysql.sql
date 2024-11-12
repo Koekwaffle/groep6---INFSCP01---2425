18b8fcba1c54fe386a51fe8f2c7051cb6d7de958
BLUEHXZE <144699673+BLUEHXZE@users.noreply.github.com>
2024-11-12 10:34:09 +0100

    database complete
    all the data from the JSON files have been transferred and the relations have been set in place

--- mysql.sql
+++ mysql.sql
@@ -55,21 +55,53 @@
 -- );
 
 -- Step 1: Create a new table without `created_at` and `updated_at`
-CREATE TABLE contacts_new (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    warehouse_id INTEGER NOT NULL,
-    name TEXT,
-    phone TEXT,
-    email TEXT,
-    FOREIGN KEY (warehouse_id) REFERENCES warehouses(id)
-);
+-- CREATE TABLE contacts_new (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     warehouse_id INTEGER NOT NULL,
+--     name TEXT,
+--     phone TEXT,
+--     email TEXT,
+--     FOREIGN KEY (warehouse_id) REFERENCES warehouses(id)
+-- );
 
--- Step 2: Copy data from the old table to the new table
-INSERT INTO contacts_new (id, warehouse_id, name, phone, email)
-SELECT id, warehouse_id, name, phone, email FROM contacts;
+-- -- Step 2: Copy data from the old table to the new table
+-- INSERT INTO contacts_new (id, warehouse_id, name, phone, email)
+-- SELECT id, warehouse_id, name, phone, email FROM contacts;
 
--- Step 3: Drop the original table
-DROP TABLE contacts;
+-- -- Step 3: Drop the original table
+-- DROP TABLE contacts;
 
--- Step 4: Rename the new table to the original table name
-ALTER TABLE contacts_new RENAME TO contacts;
+-- -- Step 4: Rename the new table to the original table name
+-- ALTER TABLE contacts_new RENAME TO contacts;
+
+-- CREATE TABLE inventories_new (
+--     id INTEGER PRIMARY KEY,
+--     item_id TEXT,
+--     description TEXT,
+--     item_reference TEXT,
+--     location_id INTEGER,  -- New foreign key column to link to locations table
+--     total_on_hand INTEGER,
+--     total_expected INTEGER,
+--     total_ordered INTEGER,
+--     total_allocated INTEGER,
+--     total_available INTEGER,
+--     created_at TEXT,
+--     updated_at TEXT,
+--     FOREIGN KEY (location_id) REFERENCES locations(id)
+-- );
+
+-- INSERT INTO inventories_new (id, item_id, description, item_reference, total_on_hand, total_expected, total_ordered, total_allocated, total_available, created_at, updated_at)
+-- SELECT id, item_id, description, item_reference, total_on_hand, total_expected, total_ordered, total_allocated, total_available, created_at, updated_at
+-- FROM inventories;
+
+-- DROP TABLE inventories;
+
+-- ALTER TABLE inventories_new RENAME TO inventories;
+
+-- CREATE TABLE IF NOT EXISTS inventory_locations (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     inventory_id INTEGER NOT NULL,  -- References an inventory item
+--     location_id INTEGER NOT NULL,   -- References a location
+--     FOREIGN KEY (inventory_id) REFERENCES inventories(id),
+--     FOREIGN KEY (location_id) REFERENCES locations(id)
+-- );
