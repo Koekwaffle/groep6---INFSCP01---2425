d690287788dcf98bd1c16862546637dcf365b660
ThomJochems <144890947+Koekwaffle@users.noreply.github.com>
2024-11-26 08:58:16 +0100

    Revert "last of today checkpoint 00:00"
    This reverts commit c81b7b63959dddd52c37b4d2a045d98f0e98c86e.

--- data/dbconn.py
+++ data/dbconn.py
@@ -5,7 +5,8 @@ def get_db_connection():
     # Get the absolute path to the project root folder dynamically
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     
-    db_path = os.path.join(project_root, 'ILY.db')
+    # Define the path for the database file relative to the project root
+    db_path = os.path.join(project_root, 'database.db')
     
     conn = sqlite3.connect(db_path)
     return conn
