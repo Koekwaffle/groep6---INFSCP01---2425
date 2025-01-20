03ab1ae9617b1833e4bff5c35ca1cd2da8d959bb
Aidanvb5 <152182270+Aidanvb5@users.noreply.github.com>
2025-01-21 00:09:22 +0100

    try out

--- endpoints_tests.py
+++ endpoints_tests.py
@@ -7,7 +7,7 @@ import requests
 import pytest
 
 BASE_URL = 'http://127.0.0.1:8000'
-API_KEY = "a1b2c3d4e5"
+API_KEY = "DYXcU3AshPCpUC0PzM1BMDnUbcoroGZm"
 
 # Use a very large ID that won't conflict with existing records
 TEST_ID = 1
